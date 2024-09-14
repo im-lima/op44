db621f4a8e43157bb1ba9be59aedbcf20c4ccdec
im-lima <halimakubulbekova@gmail.com>
2024-09-14 20:02:59 +0600

    i

--- lessons/lesson5.py
+++ lessons/lesson5.py
@@ -2,26 +2,24 @@
 import random,time,sqlite3
 # 1 вид встроенные\внутренний модули
 
-import math
+# import math
 # print(math.pi)
 
 from math import e,pi,sin,sqrt
 # print(e)
 
-
-
 # собвственные модули
 
-from lesson4_2 import Remanga
+# from lesson4_2 import Remanga
 import colorama
 print(colorama.Back.LIGHTYELLOW_EX,colorama.Fore.RED)
-
-n=Remanga('огненный кулак')
-n.manga_upp()
-
-
-# venv
-# внешние модули
-
-import colorama
+#
+# n=Remanga('огненный кулак')
+# n.manga_upp()
+#
+#
+# # venv
+# # внешние модули
+#
+# import colorama
 print('привет')
